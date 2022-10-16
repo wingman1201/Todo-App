9dccfd034e16386c224934bc6ded732d143e8ffa
wingman1201 <101256463+wingman1201@users.noreply.github.com>
2022-10-16 13:29:25 +0530

    Add files via upload

---
+++ Source Code/src/App.js
@@ -0,0 +1,13 @@
+import  Todo from './components/Todo';
+import List from './components/List';
+import './App.css';
+
+function App() {
+  return (
+    <div className="todo-app">
+      <List/>
+    </div>
+  );
+}
+
+export default App;
